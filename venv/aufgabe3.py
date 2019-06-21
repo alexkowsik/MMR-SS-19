da3c05f0700f729993a31782c9814669a75e0425
Alex <globalanekdote@web.de>
2019-06-21 20:47:01 +0200

    minor changes

--- venv/aufgabe3.py
+++ venv/aufgabe3.py
@@ -14,14 +14,14 @@ pos, = ax.plot([], [], "o", color="black")
 text = ax.text(0.1, 0.2, '', transform=ax.transAxes)
 lin, = ax.plot([], [], 'r-', linewidth=3)
 
-l = 2 # Seillänge
+l = 3 # Seillänge
 h = 1 # Schrittweite
-c = 0.95 # Dämpfungskonstante
+c = np.random.uniform(0.92, 1) # Dämpfungskonstante
 time = np.linspace(0, 100, 1000)
 
 # Zufällige startauslenkung/-Geschwindigkeit
 init_alpha = 0
-init_alphaDeriv = np.random.uniform(-20, 20)
+init_alphaDeriv = np.random.uniform(50)
 
 # In alpha stehen die Auslenkungen, in alphaDeriv die Ableitungen davon
 alpha = np.zeros(len(time))
@@ -54,14 +54,18 @@ def step(t):
     global l
 
     # In pos steht die Position des Kopfes berechnet aus den Auslekungen (ins Bogenmaß umgewandelt)
-    pos.set_data([np.cos(radians(alpha[t] - 90)) * l], [np.sin(radians(alpha[t] - 90)) * l])
+    pos.set_data([np.cos(radians(alpha[t] - 90)) * l], 
+                 [np.sin(radians(alpha[t] - 90)) * l])
     text.set_text(str(t))
-    lin.set_data([originPoint[0], np.cos(radians(alpha[t] - 90)) * l], [originPoint[1], np.sin(radians(alpha[t] - 90)) * l])
+    lin.set_data([originPoint[0], np.cos(radians(alpha[t] - 90)) * l], 
+                 [originPoint[1], np.sin(radians(alpha[t] - 90)) * l])
 
     return pos, text, lin
 
-ani = animation.FuncAnimation(fig, step, np.arange(1, len(time)), interval=50, blit=True, init_func=init)
+ani = animation.FuncAnimation(fig, step, np.arange(1, len(time)), 
+                              interval=50, blit=True, init_func=init)
 
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 plt.show()
+
